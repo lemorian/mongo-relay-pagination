eb9e3833d4e2fe9050bdb1551dcee52e7c11a05e
Arun Shankar <arun@lemorian.tech>
2021-03-30 17:58:31 +0530

    implemented Find with Connection API

--- examples/main.go
+++ examples/main.go
@@ -2,17 +2,93 @@ package main
 
 import (
 	"context"
+	"log"
 	"time"
 
+	relay "github.com/lemorian/go-relay-connection"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type Subject struct {
+	ID       string `json:"id" bson:"_id"`
+	Name     string `json:"name" bson:"name"`
+	Overview string `json:"overview" bson:"overview"`
+}
+type SubjectEdge struct {
+	Node   Subject `json:"node" bson:"node,omitempty"`
+	Cursor string  `json:"cursor" bson:"cursor,omitempty"`
+}
+
+type SubjectConnection struct {
+	Edges      []SubjectEdge  `json:"edges" bson:"edges"`
+	PageInfo   relay.PageInfo `json:"pageInfo" bson:"pageInfo,omitempty"`
+	TotalCount int64          `json:"totalCount" bson:"totalCount"`
+}
+
+func (connection *SubjectConnection) CreateEdge(decoder func(val interface{}) error) (string, error) {
+	var subject *Subject = &Subject{}
+	err := decoder(subject)
+	if err != nil {
+		return "", err
+	}
+	connection.Edges = append(connection.Edges, SubjectEdge{
+		Node:   *subject,
+		Cursor: subject.ID,
+	})
+	return subject.ID, nil
+}
+
+func (connection *SubjectConnection) SetTotalCount(count int64) {
+	connection.TotalCount = count
+}
+
+func (connection *SubjectConnection) SetPageInfo(pageInfo relay.PageInfo) {
+	connection.PageInfo = pageInfo
+}
+
 func main() {
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	_, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
 		panic(err)
 	}
 
+	var db = client.Database("dev")
+
+	var collection = db.Collection("subjects")
+
+	var subjectCollection *SubjectConnection = &SubjectConnection{}
+
+	paginator := relay.Paginator{
+		Collection: collection,
+		Ctx:        ctx,
+		ConCreator: subjectCollection,
+		PagingOptions: relay.Options{
+			First: 5,
+			After: "605db89d208db95eb4878553",
+			Filter: &bson.M{
+				"overview": "Tuesday",
+			},
+		},
+	}
+
+	err = paginator.Find()
+
+	if err != nil {
+		log.Println("error", err.Error())
+	}
+
+	// for _, result := range results {
+	// 	if subject, ok := result.(SubjectEdge); ok {
+	// 		subjects = append(subjects, subject)
+	// 	}
+	// }
+	// if err != nil {
+	// 	log.Println("error", err.Error())
+	// }
+
+	log.Printf("results %v", subjectCollection.PageInfo.HasNextPage)
 }
---
+++ relay.go
@@ -0,0 +1,201 @@
+package relay
+
+import (
+	"context"
+	"errors"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// Error constants
+const (
+	PageLimitError         = "first and last cannot be less than 0"
+	DecodeEmptyError       = "struct should be provide to decode data"
+	DecodeNotAvail         = "this feature is not available for aggregate query"
+	FilterInAggregateError = "you cannot use filter in aggregate query but you can pass multiple filter as param in aggregate function"
+	NilFilterError         = "filter query cannot be nil"
+)
+
+type PageInfo struct {
+	HasNextPage     bool    `json:"hasNextPage" bson:"hasNextPage"`
+	HasPreviousPage bool    `json:"hasPreviousPage" bson:"hasPreviousPage"`
+	StartCursor     *string `json:"startCursor" bson:"startCursor,omitempty"`
+	EndCursor       *string `json:"endCursor" bson:"endCursor,omitempty"`
+}
+
+type RelayConnectionCreator interface {
+	CreateEdge(cursorDecoder func(val interface{}) error) (string, error)
+	SetTotalCount(int64)
+	SetPageInfo(PageInfo)
+}
+
+//PagionationOptions - Arguments received from the GraphQL Client
+type Options struct {
+	First   int64
+	Last    int64
+	After   string
+	Before  string
+	Filter  *bson.M
+	Project *bson.M
+}
+type Paginator struct {
+	Collection    *mongo.Collection
+	Ctx           context.Context
+	ConCreator    RelayConnectionCreator
+	PagingOptions Options
+}
+
+func mergeFilters(filters ...bson.M) bson.M {
+	var mergedFilter = bson.M{}
+	for _, filter := range filters {
+		for key, element := range filter {
+			mergedFilter[key] = element
+		}
+	}
+	return mergedFilter
+}
+
+// validateQuery query is to check if user has added certain required params or not
+func (paginator *Paginator) validateQuery(isNormal bool) error {
+	if paginator.PagingOptions.First <= 0 && paginator.PagingOptions.Last <= 0 {
+		return errors.New(PageLimitError)
+	}
+	if isNormal && paginator.ConCreator == nil {
+		return errors.New(DecodeEmptyError)
+	}
+	if !isNormal && paginator.ConCreator != nil {
+		return errors.New(DecodeNotAvail)
+	}
+	return nil
+}
+
+func (paginator *Paginator) getContext() context.Context {
+	if paginator.Ctx != nil {
+		return paginator.Ctx
+	} else {
+		return context.Background()
+	}
+}
+
+func (paginator *Paginator) setTotal() error {
+
+	ctx := paginator.getContext()
+	var total int64
+	var err error
+	if paginator.PagingOptions.Filter != nil {
+		total, err = paginator.Collection.CountDocuments(ctx, paginator.PagingOptions.Filter)
+	} else {
+		total, err = paginator.Collection.CountDocuments(ctx, bson.M{})
+	}
+	if err != nil {
+		return err
+	}
+	paginator.ConCreator.SetTotalCount(total)
+	return nil
+}
+
+func (paginator *Paginator) hasMore(cursorFilter bson.M, searchFilter bson.M, limit int64) (bool, error) {
+	ctx := paginator.getContext()
+	var mergedFilter = mergeFilters(cursorFilter, searchFilter)
+
+	resultCount, err := paginator.Collection.CountDocuments(ctx, mergedFilter)
+
+	if err != nil {
+		return false, err
+	}
+
+	if resultCount >= limit {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func (paginator *Paginator) Find() error {
+
+	if err := paginator.validateQuery(true); err != nil {
+		return err
+	}
+
+	opts := &options.FindOptions{}
+	var hasNextPageLimit int64
+
+	if paginator.PagingOptions.First > 0 {
+		opts.SetLimit(paginator.PagingOptions.First)
+		hasNextPageLimit = paginator.PagingOptions.First + 1
+	} else {
+		opts.SetLimit(paginator.PagingOptions.Last)
+		hasNextPageLimit = paginator.PagingOptions.Last + 1
+	}
+
+	var cursorFilter bson.M = bson.M{}
+	var cursorFilterReverse bson.M = bson.M{}
+
+	//If Both after and before is set, only after is considered
+	if len(paginator.PagingOptions.After) > 0 {
+		oid, err := primitive.ObjectIDFromHex(paginator.PagingOptions.After)
+		if err != nil {
+			return err
+		}
+		cursorFilter["_id"] = bson.M{"$gt": oid}
+		cursorFilterReverse["_id"] = bson.M{"$lt": oid}
+	} else if len(paginator.PagingOptions.Before) > 0 {
+		oid, err := primitive.ObjectIDFromHex(paginator.PagingOptions.Before)
+		if err != nil {
+			return err
+		}
+		cursorFilter["_id"] = bson.M{"$lt": oid}
+		cursorFilterReverse["_id"] = bson.M{"$gt": oid}
+	}
+
+	ctx := paginator.getContext()
+
+	err := paginator.setTotal()
+
+	if err != nil {
+		return err
+	}
+
+	var mergedFilter = mergeFilters(cursorFilter, *paginator.PagingOptions.Filter)
+
+	mgoCursor, err := paginator.Collection.Find(ctx, mergedFilter, opts)
+
+	if err != nil {
+		return err
+	}
+
+	var pageInfo PageInfo = PageInfo{}
+	var currentCursor string
+	for mgoCursor.Next(ctx) {
+
+		currentCursor, err := paginator.ConCreator.CreateEdge(mgoCursor.Decode)
+
+		if err != nil {
+			return err
+		}
+
+		if pageInfo.StartCursor == nil {
+			pageInfo.StartCursor = &currentCursor
+		}
+	}
+	pageInfo.EndCursor = &currentCursor
+
+	pageInfo.HasNextPage, err = paginator.hasMore(cursorFilter, *paginator.PagingOptions.Filter, hasNextPageLimit)
+
+	if err != nil {
+		return err
+	}
+
+	pageInfo.HasPreviousPage, err = paginator.hasMore(cursorFilterReverse, *paginator.PagingOptions.Filter, 1)
+
+	if err != nil {
+		return err
+	}
+
+	paginator.ConCreator.SetPageInfo(pageInfo)
+
+	return nil
+}
